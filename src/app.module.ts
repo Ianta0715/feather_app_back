ef08d5645183ff1636a8a6ee4b57997f26b62bc7
Ian Tarquini <ian@continentalvc.com>
2025-10-03 12:57:19 -0300

    change the variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { Budget } from './budgets/budget.entity';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: parseInt(process.env.DB_PORT || '3306'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
